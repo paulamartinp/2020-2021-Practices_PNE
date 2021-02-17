31cef3f35c9a0df7d13cffe9698ee2bb3fb7c6fa
paulamartinp <p.martinp.2020alumnos.urjc.es>
2021-02-17 12:49:05 +0100

    Exercise 2 Fibonacci

---
+++ Fibonacci Exercises/fibonacci2.py
@@ -0,0 +1,8 @@
+import math
+def fibon(n):
+    number = int((((1 + math.sqrt(5)) / 2) ** n - ((1 - math.sqrt(5)) / 2) ** n) // math.sqrt(5))
+    return str(n) + "th Fibonacci term: " + str(number)
+
+print(fibon(5))
+print(fibon(10))
+print(fibon(15))
